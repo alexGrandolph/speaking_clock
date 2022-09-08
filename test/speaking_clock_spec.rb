7ad6eed804db177106b04a45c603d6493ce7af80
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-08 12:32:04 -0600

    test: add test for returning time past the hour up to 25 past"

--- test/speaking_clock_spec.rb
+++ test/speaking_clock_spec.rb
@@ -2,13 +2,13 @@ require 'rspec'
 require './lib/speaking_clock'
 require 'pry'
 RSpec.describe 'Speaking Clock Challenge' do
+  
   it 'Returns the correct time on the hour' do
 
     time1 = '1:00'
     time2 = '8:00'
     time3 = '11:00'
 
-    
     speaker1 = SpeakingClock.speak(time1)
     speaker2 = SpeakingClock.speak(time2)
     speaker3 = SpeakingClock.speak(time3)
@@ -17,4 +17,23 @@ RSpec.describe 'Speaking Clock Challenge' do
     expect(speaker2).to eq("eight o'clock")
     expect(speaker3).to eq("eleven o'clock")
   end
+  
+  
+  it 'Returns time past the hour (not including quarter) up to 25 minutes past' do
+
+    time1 = '4:05'
+    time2 = '3:10'
+    time3 = '5:20'
+    time3 = '6:25'
+
+    speaker1 = SpeakingClock.speak(time1)
+    speaker2 = SpeakingClock.speak(time2)
+    speaker3 = SpeakingClock.speak(time3)
+
+    expect(speaker1).to eq("five past four")
+    expect(speaker2).to eq("ten past three")
+    expect(speaker3).to eq("twenty past five")
+    expect(speaker4).to eq("twenty five past six")
+  end
+
 end
