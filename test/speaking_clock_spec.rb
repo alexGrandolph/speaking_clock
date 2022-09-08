05cf796d7d50a41e56c44a7eb5fce4a9e12fc62e
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-08 12:27:59 -0600

    refactor/test: refactor first test to only test for returning on the hour times

--- test/speaking_clock_spec.rb
+++ test/speaking_clock_spec.rb
@@ -2,24 +2,20 @@ require 'rspec'
 require './lib/speaking_clock'
 require 'pry'
 RSpec.describe 'Speaking Clock Challenge' do
-  it 'Returns the correct string for given digit time' do
+  it 'Returns the correct time on the hour' do
 
     time1 = '1:00'
-    time2 = '2:05'
-    time3 = '3:10'
-    time4 = '6:25'
+    time2 = '8:00'
+    time3 = '11:00'
 
     speaker = SpeakingClock.new
     speaker1 = speaker.speak(time1)
     speaker2 = speaker.speak(time2)
     speaker3 = speaker.speak(time3)
-    speaker4 = speaker.speak(time4)
 
     expect(speaker).to be_a SpeakingClock
     expect(speaker1).to eq("one o'clock")
-    expect(speaker2).to eq("five past two")
-    expect(speaker3).to eq("ten past three")
-    expect(speaker4).to eq("twenty five past six")
-
+    expect(speaker2).to eq("eight o'clock")
+    expect(speaker3).to eq("eleven o'clock")
   end
 end
