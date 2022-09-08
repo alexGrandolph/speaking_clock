c3f348e1607d538f336d5024df31ac51026468de
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-08 13:15:50 -0600

    fix: adjust whitespace

--- lib/speaking_clock.rb
+++ lib/speaking_clock.rb
@@ -8,10 +8,11 @@ class SpeakingClock
 
   def self.speak(digits)
     diggit_arr = digits.split(":")
-    
+   
     if diggit_arr.last.end_with?('0')  || diggit_arr.last.end_with?('5') 
       if diggit_arr.last == "00"
         double_zeros(diggit_arr)
+
       elsif diggit_arr.last.to_i <= 25
         past_the_hour(diggit_arr)
       
@@ -23,6 +24,7 @@ class SpeakingClock
       first_string = "#{re_humanize(diggit_arr[0].to_i)}"
       second_string = "#{re_humanize(diggit_arr.last.to_i)}"
       "#{first_string} #{second_string}"
+
     end
 
   end 
@@ -32,10 +34,12 @@ class SpeakingClock
     def self.past_the_hour(diggit_arr)
       if diggit_arr.last == "15"
         "quarter past #{re_humanize(diggit_arr[0].to_i)}"
+
       else  
         first_string = "#{re_humanize(diggit_arr.last.to_i)}"
         second_string = "past #{re_humanize(diggit_arr[0].to_i)}"
         "#{first_string} #{second_string}"
+
       end   
     end 
 
@@ -43,25 +47,29 @@ class SpeakingClock
       hour = diggit_arr[0].to_i + 1
       if diggit_arr.last == "45"
         "quarter to #{re_humanize(hour)}"
+
       else  
         minutes_until = 60 - diggit_arr.last.to_i
         first_string = "#{re_humanize(minutes_until)}"
         second_string = "to #{re_humanize(hour)}"
         "#{first_string} #{second_string}"
+
       end 
     end 
 
     def self.double_zeros(diggit_arr)
       if diggit_arr[0] == '00'
         "midnight"
+
       elsif diggit_arr[0] == '12'
         "noon"
+
       else
         first_string = "#{re_humanize(diggit_arr[0].to_i)}"
         second_string = "o'clock"
         "#{first_string} #{second_string}"
-      end   
 
+      end   
     end 
   
 end
